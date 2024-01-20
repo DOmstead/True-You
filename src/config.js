c6007c9795df8929df5ce82098de64c27940ddde
David Omstead <49734565+DOmstead@users.noreply.github.com>
2024-01-19 17:30:22 -0700

    Host service change.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@
 
 export default {
   //This is the API Point for local development prior to production.
-  // API_ENDPOINT: `http://localhost:8800/api/namerecords`,
-  API_ENDPOINT: `https://true-you-89a4870e34f8.herokuapp.com/api/namerecords`,
+  API_ENDPOINT: `https://true-you-api.onrender.com/api/namerecords`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
